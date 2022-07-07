12f6a302caed452c3cf877fc163a353beccf57f8
Gabriel <gabriel.polidoro@acad.ftec.com.br>
2022-07-07 08:59:30 -0300

    funcional

--- imc_calculator.js
+++ imc_calculator.js
@@ -1,15 +1,22 @@
-const personName = document.getElementById('Name_zur');
-const calculate  = document.getElementById('submit')
 
-function reciveName(){
-
-    if(reciveName === undefined || reciveName === null){
-        console.log('Digite um valor valido')
-    }else{
-        console.log(personName)
-    }
-
+function armazenaVariavel(){
     
+    const personName = document.getElementById('Name').value 
+    
+    const personHeight = document.getElementById('Height').value
+
+    const personWeight = document.getElementById('Weight').value
+
+    const result = personWeight / (personHeight * personHeight)
+
+
+    document.getElementById('name_visor').innerHTML = "Name: " + personName
+
+    document.getElementById('height_visor').innerHTML = "Height: " + personHeight
+
+    document.getElementById('weight_visor').innerHTML = "Weight: " + personWeight
+    
+    document.getElementById('result_visor').innerHTML = "IMC: " + result.toFixed(2)
+
 }
 
-calculate.addEventListener('click', reciveName)
